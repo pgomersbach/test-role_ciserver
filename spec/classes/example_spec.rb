58bfab127bbcc384b47b8b948bc5727301118c72
Paul Gomersbach <p.gomersbach@rely.nl>
2015-06-23 17:27:21 +0200

    add default role to spec tests

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -17,6 +17,7 @@ describe 'role_ciserver' do
        
           it { is_expected.to contain_class('profile_base') }
           it { is_expected.to contain_class('profile_base::rspec_monitor') }
+          it { is_expected.to contain_class('role_ciserver') }
 
         end
       end
